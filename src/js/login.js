aa0adbfd5ec712b98614a7c702c769a126938c3c
YY <z3388638@gmail.com>
2017-03-04 22:45:55 +0800

    add login page

---
+++ src/js/login.js
@@ -0,0 +1,34 @@
+var Login = (_ => {
+
+	/**
+	 * cache DOM 
+	**/
+	const $loginCard = $('#login');
+	const $window = $(window);
+
+	/**
+	 * bind event
+	**/
+	$loginCard.on('click.login', '#btn-login', _handleLogin);
+	$loginCard.on('keydown.login', 'input', _handleLogin);
+	$window.on('resize', _handleResize);
+
+	function _handleLogin(e) {
+		if(e.type == 'keydown' && e.keyCode != 13) {
+			return;
+		}
+		const account = $loginCard.find('#account').val();
+		const password = $loginCard.find('#password').val();
+		console.log(account);
+		console.log(password);
+	}
+
+	function _handleResize() {
+		const H = $(this).height();
+		const mt = (H - 202) / 2;
+		$loginCard.css('margin-top', `${mt}px`);
+	}
+
+	_handleResize();
+
+})();
