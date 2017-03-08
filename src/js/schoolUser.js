2834d841f9bc8f957ea78438021be190d137ea85
YY <z3388638@gmail.com>
2017-03-08 22:06:20 +0800

    render list of school user

--- src/js/schoolUser.js
+++ src/js/schoolUser.js
@@ -7,6 +7,13 @@ var SchoolUser = (_ => {
 	$listTable = $schoolUserPage.find('#schoolUserListTable');
 
 	/**
+	 * init
+	 */
+	_checkPermission();
+	_handleResize();
+	_getSchoolUserList();
+
+	/**
 	 * bind event
 	 */
 	$titleRow.on('click.addSchoolUser', '.btn-openAddSchoolUserModal', _addSchoolUser);
@@ -27,6 +34,37 @@ var SchoolUser = (_ => {
 		console.log('deleteeee');
 	}
 
+	function _getSchoolUserList() {
+		// TODO: get list
+		var list = [
+			{
+				name: '蔡英文', 
+				emali: 'abcde@gmail.com', 
+				org: '海外聯招', 
+				tel: '1234567#1234'
+			}
+		]
+
+		$listTable
+			.find('tbody')
+			.append(list.map((val, i) => $(
+				`<tr class="schoolUser">
+					<td>${val.name}</td>
+					<td>${val.email}</td>
+					<td>${val.org}</td>
+					<td>${val.tel}</td>
+					<td>
+						<button class="btn btn-warning btn-edit">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</button> 
+						<button class="btn btn-danger btn-delete">
+							<i class="fa fa-trash-o" aria-hidden="true"></i>
+						</button>
+					</td>
+				</tr>`
+		)));
+	}
+
 	function _handleResize() {
 		const windowH = $(this).height();
 		let listH = windowH - 250;
@@ -34,6 +72,8 @@ var SchoolUser = (_ => {
 		$schoolUserPage.find('#schoolUserList').css('height', `${listH}px`);
 	}
 
-	_handleResize();
+	function _checkPermission() {
+		// TODO: checkPermission
+	}
 
 })();
