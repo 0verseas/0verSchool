a181336a1f399c05d8ed3e5ff3c3c3f5c04015a2
YY <z3388638@gmail.com>
2017-03-25 20:34:24 +0800

    add regulation page

---
+++ src/js/regulation.js
@@ -0,0 +1,20 @@
+var Regulation = (_ => {
+	/**
+	 * cache DOM
+	 */
+	$regulationPage = $('#page-regulation');
+
+	/**
+	 * init
+	 */
+	_getSchoolInfo();
+
+	/**
+	 * bind event
+	 */
+	
+	function _getSchoolInfo() {
+
+	}
+	
+})();
