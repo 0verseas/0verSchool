b116e66fdd7513ef8a72770cf472b8a0bd32613f
YY <z3388638@gmail.com>
2017-04-04 02:11:02 +0800

    department form structure

--- src/js/regulation.js
+++ src/js/regulation.js
@@ -3,6 +3,7 @@ var Regulation = (_ => {
 	 * cache DOM
 	 */
 	$regulationPage = $('#page-regulation');
+	$titles = $regulationPage.find('.title');
 	$schoolForm = $regulationPage.find('#schoolForm');
 	$saveBtn = $schoolForm.find('#btn-save');
 	$resetBtn = $schoolForm.find('#btn-reset');
@@ -17,13 +18,21 @@ var Regulation = (_ => {
 	 */
 	$saveBtn.on('click', _handleSave);
 	$resetBtn.on('click', _getSchoolInfo);
+	$titles.on('click', _handleToggleContent);
 	
 	function _getSchoolInfo() {
 
 	}
 
 	function _handleSave() {
-		
+
+	}
+
+	function _handleToggleContent() {
+		let $this = $(this);
+		let $target = $regulationPage.find(`#${$this.data('content')}`);
+		$this.toggleClass('active');
+		$target.slideToggle();
 	}
 	
 })();
