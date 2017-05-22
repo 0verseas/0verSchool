7cd82b7d99829ede28030443cb4bf525648fe5e5
lemon5920 <lemon5920@gmail.com>
2017-05-22 17:41:59 +0800

    增加審查項目。

--- js/deptInfoBache.js
+++ js/deptInfoBache.js
@@ -10,6 +10,8 @@ var deptInfoBache = (function () {
 	$bachelorTotalPeople = $('#bachelorTotalPeople');
 	$bachelorPersonalApply = $('#bachelorPersonalApply');
 	$bachelorDistribution = $('#bachelorDistribution');
+	$addReviewItemBtn = $('#btn-addReviewItem');
+	$reviewItemsForm = $('#form-reviewItems');
 
 	/**
 	 * bind event
@@ -19,8 +21,9 @@ var deptInfoBache = (function () {
 	$editDeptInfoBtn.on('click', _handleEditDeptInfo);
 	$bachelorPersonalApply.on('keyup', _computeBachelorTotalPeople);
 	$bachelorDistribution.on('keyup', _computeBachelorTotalPeople);
+	$addReviewItemBtn.on('click', _addReviewItemRow);
 
-	function _filterDeptInput(e){
+	function _filterDeptInput(e) {
 		let filter = $deptFilterInput.val().toUpperCase();
 		var tr = $deptList.find('tr');
 
@@ -38,13 +41,52 @@ var deptInfoBache = (function () {
 		}
 	}
 
-	function _handleEditDeptInfo(){
+	function _handleEditDeptInfo() {
 		$editDeptInfoModal.modal();
 	}
 
 	function _computeBachelorTotalPeople() {
 		var totalPeople = Number($bachelorPersonalApply.val()) + Number($bachelorDistribution.val());
 		$bachelorTotalPeople.val(totalPeople);
+	}
+
+	function _addReviewItemRow() {
+		var $newReviewItemRow = $(`
+			<div class="row">
+				<div class="form-group col-sm-4">
+					<label for="">審查項目</label>
+
+					<select class="form-control">
+						<optgroup label="分類？">
+							<option>畢業證書</option>
+							<option>作品</option>
+						</optgroup>
+						<optgroup label="分類？">
+							<option>其他</option>
+						</optgroup>
+					</select>
+				</div>
+
+				<div class="col-sm-8">
+					<div class="form-check">
+						<label class="form-check-label">
+						<input type="checkbox" class="form-check-input">
+						是否為必備
+						</label>
+					</div>
+					<div class="form-group">
+						<label for="">審查項目中文說明</label>
+						<textarea class="form-control" rows="3"></textarea>
+					</div>
+
+					<div class="form-group">
+						<label for="">審查項目英文說明</label>
+						<textarea class="form-control" rows="3"></textarea>
+					</div>
+				</div>
+			</div>
+			<hr>`);
+		$reviewItemsForm.append($newReviewItemRow);
 	}
 
 })();
