b1f1c70d2a2f153bec5702fdc0ebdb3bff095fe7
YY <z3388638@gmail.com>
2017-05-25 19:39:40 +0800

    get qouta via api; set quota status badge

--- js/quotaDistributionBache.js
+++ js/quotaDistributionBache.js
@@ -3,6 +3,7 @@ var quotaDistirbutionBache = (function () {
 	 * cacheDOM
 	 */
 	var $page = $('#pageContent');
+	var $statusBadge = $page.find('#badge-status');
 
 	/**
 	 * bind event
@@ -13,5 +14,45 @@ var quotaDistirbutionBache = (function () {
 	 */
 	// show bache only
 	$page.find('.bacheOnly').removeClass('hide');
+	_setData();
+
+	function _setData() {
+		fetch('https://api.overseas.ncnu.edu.tw/schools/me/systems/bachelor/histories/latest?data_type=quota', {
+			credentials: 'include'
+		}).then(function (res) {
+			if(res.ok) {
+				return res.json();
+			} else {
+				throw res
+			}
+		}).then(function (json) {
+			console.log(json);
+			_setStatus(json.quota_status);
+			_setQuota(json);
+			_setDeptList(json.departments);
+		}).catch(function (err) {
+			console.error(err);
+		});
+	}
+
+	function _setStatus(status) {
+		switch (status) {
+			case 'waiting':
+				break;
+			case 'confirmed':
+				break;
+			case 'editing':
+				$statusBadge.addClass('badge-danger').text('編輯中');
+				break;
+		}
+	}
+
+	function _setQuota() {
+
+	}
+
+	function _setDeptList() {
+
+	}
 
 })();
