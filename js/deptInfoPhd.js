899b9a00b2900bb989e05b2678317d3590ed27d4
lemon5920 <lemon5920@gmail.com>
2017-06-20 16:20:14 +0800

    增加 學制博士班、二技備註 目前測試用 editor 沒有二技資料， 因此不確定二技 system_id 為 twoYear 正不正確。

---
+++ js/deptInfoPhd.js
@@ -0,0 +1,61 @@
+var deptInfoPhd = (function () {
+	/**
+	 * cache DOM
+	 */
+	var $deptInfoForm = $('#form-deptInfo');
+	var $deptInfoDescription = $deptInfoForm.find('#description');
+	var $deptInfoEngDescription = $deptInfoForm.find('#engDescription');
+	var $deptInfoBtn = $deptInfoForm.find('#btn-deptInfoSave, #btn-deptInfoCommit');
+
+	/**
+	 * bind event
+	 */
+	$deptInfoBtn.on('click', _handleDeptInfoSaveOrCommit);
+
+	/**
+	 * init
+	 */
+
+	_setData();
+
+	function _setData() {
+		School.getSystemInfo('phd')
+		.then(function (res) {
+			if(res.ok) {
+				return res.json();
+			} else {
+				throw res
+			}
+		}).then(function (json) {
+			console.log(json);
+			$deptInfoDescription.text(json.description);
+			$deptInfoEngDescription.text(json.eng_description);
+		}).catch(function (err) {
+			console.error(err);
+		})
+	}
+
+	function _handleDeptInfoSaveOrCommit() {
+		var action = $(this).data('action');
+		var data = {
+			'action': action,
+			'description': $deptInfoDescription.val(),
+			'eng_description': $deptInfoEngDescription.val()
+		}
+
+		School.setSystemInfo('phd', data)
+		.then(function (res) {
+			if(res.ok) {
+				alert('儲存成功')
+				return res.json();
+			} else {
+				alert('儲存失敗')
+				throw res
+			}
+		}).then(function (json) {
+			console.log(json);
+		}).catch(function (err) {
+			console.error(err);
+		});
+	}
+})();
