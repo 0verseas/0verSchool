dd46861b38359d962d0958cfe5cc37347d1f6e1e
lemon5920 <lemon5920@gmail.com>
2017-06-28 12:22:38 +0800

    學制頁面系所列表接上 API。

--- js/deptInfoMaster.js
+++ js/deptInfoMaster.js
@@ -7,6 +7,8 @@ var deptInfoMaster = (function () {
 	var $deptInfoEngDescription = $deptInfoForm.find('#engDescription');
 	var $deptInfoBtn = $deptInfoForm.find('#btn-deptInfoSave, #btn-deptInfoCommit');
 
+	var $deptList = $('#dept-list');
+
 	/**
 	 * bind event
 	 */
@@ -18,18 +20,59 @@ var deptInfoMaster = (function () {
 
 	_setData();
 
+	function _setDeptList(departments) {
+		// 預設排序
+		departments.sort(function (a, b) {
+		  return a.sort_order - b.sort_order;
+		});
+
+		// 狀態名稱對應中文
+		var infoStatus = {
+			editing: '編輯中',
+			returned: '退回',
+			confirmed: '通過',
+			waiting: '待審閱'
+		}
+
+		// 列表初始化
+		$deptList.find('tbody').html('');
+		departments.forEach(function (value, index) {
+			var updateAt = moment(value.creator.updated_at);
+			$deptList
+				.find('tbody')
+				.append(`
+					<tr>
+						<td><span class="btn-editDeptInfo"><i class="fa fa-pencil" aria-hidden="true"></i></span></td>
+						<td>${index}</td>
+						<td>${value.id}</td>
+						<td>
+							<div>${value.title}</div>
+							<div>${value.eng_title}</div>
+						</td>
+						<td>${infoStatus[value.info_status]}</td>
+						<td>${value.creator.name}</td>
+						<td>${updateAt.format('M月D日 H:m:s (YYYY)')}</td>
+					</tr>
+				`);
+		});
+	}
+
 	function _setData() {
 		School.getSystemInfo('master')
-		.then(function (res) {
+		.then((res) => {
 			if(res.ok) {
 				return res.json();
 			} else {
 				throw res
 			}
-		}).then(function (json) {
+		}).then((json) => {
 			$deptInfoDescription.text(json.description);
 			$deptInfoEngDescription.text(json.eng_description);
-		}).catch(function (err) {
+			_setDeptList(json.departments);
+		}).then(() => {
+			$.bootstrapSortable(true);
+		})
+		.catch((err) => {
 			console.error(err);
 		})
 	}
