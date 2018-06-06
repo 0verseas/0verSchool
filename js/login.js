3368055bff7d072b7e26cf50d4c13fa25b09cc97
He-Wen Chen <lotus3698@gmail.com>
2018-06-06 14:39:58 +0800

    fix typo

--- js/login.js
+++ js/login.js
@@ -46,7 +46,7 @@ var login = (function () {
 			}
 		}).then(function(json) {
 			console.log(json);
-			window.location.href = '/school/systemQuota'
+			window.location.href = '/school/systemQuota.html'
 		}).catch(function(err) {
 			if (err == 401) {
 				$errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
