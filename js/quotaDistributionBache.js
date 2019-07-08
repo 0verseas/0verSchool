84fa96376d39eb2fee849210309dc12fa197add3
hewen-lotus <lotus3698@gmail.com>
2019-07-08 17:18:05 +0800

    核定總量顯示 改為 欲使用名額、班別間流用

--- js/quotaDistributionBache.js
+++ js/quotaDistributionBache.js
@@ -9,6 +9,8 @@ var quotaDistirbutionBache = (function () {
 	//quota
 	var $quota_allowTotal = $page.find('.quota.allowTotal'); // 本年度可招生總量
 	var $quota_last_year_admission_amount = $page.find('.quota.last_year_admission_amount'); // 去年招生名額 * 10%
+	var $quota_used = $page.find('.quota.quota_used'); // 欲使用名額
+	var $quota_passed = $page.find('.quota.quota_passed'); // 班別間流用
 	var $quota_last_year_surplus_admission_quota = $page.find('.quota.last_year_surplus_admission_quota'); // 去年本地生招生缺額數*
 	var $quota_ratify_expanded_quota = $page.find('.quota.ratify_expanded_quota'); // 本年度教育部核准擴增名額
 	var $quota_admission_selection_quota = $page.find('.quota.admission_selection_quota'); // 學士班個人申請
@@ -205,13 +207,17 @@ var quotaDistirbutionBache = (function () {
 			another_department_admission_selection_quota,
 			another_department_self_enrollment_quota,
 			self_enrollment_quota,
-            school_has_self_enrollment
+			school_has_self_enrollment,
+			quota_used,
+			quota_passed
 		} = data;
 		$quota_last_year_admission_amount.val(last_year_admission_amount || 0);
 		$quota_last_year_surplus_admission_quota.val(last_year_surplus_admission_quota || 0);
 		$quota_ratify_expanded_quota.val(ratify_expanded_quota || 0);
 		$quota_another_department_admission_selection_quota.val(another_department_admission_selection_quota || 0);
 		$quota_another_department_self_enrollment_quota.val(another_department_self_enrollment_quota || 0);
+		$quota_used.val(quota_used || 0);
+		$quota_passed.val(quota_passed || 0);
 
         if (school_has_self_enrollment) {
             $quota_self_enrollment_quota.val(self_enrollment_quota || 0);
@@ -325,7 +331,8 @@ var quotaDistirbutionBache = (function () {
 	}
 
 	function _updateAllowTotal() {
-		var sum = +($quota_last_year_admission_amount.val()) +
+		var sum = +($quota_used.val()) +
+			+($quota_passed.val())+
 			+($quota_last_year_surplus_admission_quota.val()) +
 			+($quota_ratify_expanded_quota.val());
 		$quota_allowTotal.val(sum);
