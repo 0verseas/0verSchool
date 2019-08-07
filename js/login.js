6ef040fda89979596252991832ff934b448c55b2
hms5232 <hms5232@yahoo.com.tw>
2019-08-07 11:33:54 +0800

    登入錯誤次數太多被鎖會跳不同訊息

--- js/login.js
+++ js/login.js
@@ -51,6 +51,8 @@ var login = (function () {
 		}).catch(function(err) {
 			if (err == 401) {
 				$errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
+			} else if(err == 429){  // 429 Too Many Requests
+				$errMsg.finish().show().text('錯誤次數太多，請稍後再試。').fadeOut(3000);
 			}
 		})
 	}
