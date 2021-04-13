4516a02a25a764b4dd2b6e36ea79ee7470715967
Rick <ricklai82@gmail.com>
2021-04-13 13:13:35 +0800

    導入 google reCaptcha v3 驗證

--- js/login.js
+++ js/login.js
@@ -13,6 +13,12 @@ var login = (function () {
 	const $identifyingCode = $('#identifying-code');  // 使用者輸入的圖形驗證碼
 	let identifyingCode = '';  // 隨機產生的圖形驗證碼
 
+	// 引入 reCAPTCHA 的 JS 檔案
+    var s = document.createElement('script');
+    src = 'https://www.google.com/recaptcha/api.js?render=' + env.reCAPTCHA_site_key;
+    s.setAttribute('src', src);
+    document.body.appendChild(s);
+
 	/**
 	 * bind event
 	 */
@@ -55,26 +61,40 @@ var login = (function () {
 
 		var loginForm = {
 			username: username,
-			password: sha256(password)
+			password: sha256(password),
+			google_recaptcha_token: ''
 		}
 
-		User.login(loginForm).then(function(res) {
-			if(res.ok) {
-				return res.json();
-			} else {
-				throw res.status;
-			}
-		}).then(function(json) {
-			console.log(json);
-			window.location.href = '/school/index.html';
-			// window.location.href = '/school/systemQuota.html'
-		}).catch(function(err) {
-			if (err == 401) {
-				$errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
-			} else if(err == 429){  // 429 Too Many Requests
-				$errMsg.finish().show().text('錯誤次數太多，請稍後再試。').fadeOut(3000);
-			}
-		})
+		grecaptcha.ready(function() {
+            grecaptcha.execute(env.reCAPTCHA_site_key, {
+              action: 'SchoolLogin'
+            }).then(function(token) {
+                // token = document.getElementById('btn-login').value
+                loginForm.google_recaptcha_token=token;
+            }).then(function(){
+				User.login(loginForm).then(function (res) {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw res;
+                    }
+                }).then(function (json) {
+                    // console.log(json);
+					window.location.href = '/school/index.html';
+					// window.location.href = '/school/systemQuota.html'
+                }).catch(function (err) {
+                    console.log(err);
+                    if (err.status == 401) {
+                        $errMsg.finish().show().text('帳號密碼錯誤。').fadeOut(1500);
+                    } else if (err.status == 403) {
+                        $errMsg.finish().show().text('Google reCAPTCHA verification failed').fadeOut(5000);
+                    }
+                    else if (err.status == 429) {  // 429 Too Many Requests
+                        $errMsg.finish().show().text('錯誤次數太多，請稍後再試。').fadeOut(5000);
+                    }
+                })
+			});
+        });
 	}
 
 	function _getStatus() {
