6bdd59285e9580aaadec5502135a83b732c2f6d5
ricklai82 <ricklai82@gmail.com>
2022-09-02 11:51:56 +0800

    修改國際專修部標籤顏色

--- js/quotaDistributionPhd.js
+++ js/quotaDistributionPhd.js
@@ -328,7 +328,7 @@ var quotaDistributionPhd = (function () {
 				department_title = department_title+'&nbsp;&nbsp;<span class="badge badge-warning">重點產業系所</span>';
 			}
 			if(is_extended_department == 2){
-				department_title = department_title+'&nbsp;&nbsp;<span class="badge badge-warning">國際專修部</span>';
+				department_title = department_title+'&nbsp;&nbsp;<span class="badge table-primary">國際專修部</span>';
 			}
 
 			$deptList
